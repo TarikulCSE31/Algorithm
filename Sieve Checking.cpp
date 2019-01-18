1f4d94d445d4fa34e6bd108b65d9e2f1b89eaa83
TarikulCSE31 <39971173+TarikulCSE31@users.noreply.github.com>
2019-01-18 16:48:22 +0600

    Update Sieve Checking.cpp

--- Sieve Checking.cpp
+++ Sieve Checking.cpp
@@ -1,26 +1,41 @@
+
 /***
+
             Bismillahir Rahmanir Rahim
             Read the name of Allah, who created you!!!
-            Author : Shah Newaj Rabbi Shishir
+            Author : Tarikul Islam
             Department of CSE, City University, Bangladesh.
+
 ***/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define sf scanf
-#define pf printf
-#define scase sf ("%d",&tc)
-#define sn sf ("%d",&n)
-#define whilecase while (tc--)
-#define eof while (cin >> n)
-#define forloop for (pos=1; pos<=tc; pos++)
-#define arrayloop (i=0; i<n; i++)
-#define cinstr cin >> str
-#define getstr getline (cin,str)
-#define pcase pf ("Case %d: ",pos)
-#define pb push_back
-#define in insert
+#define sf          scanf
+#define pf          printf
+#define ssf         sscanf
+#define spf         sprintf
+#define fsf         fscanf
+#define fpf         fprintf
+#define fast        ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase       sf ("%d",&tc)
+#define sn          sf ("%d",&n)
+#define whilecase   while (tc--)
+#define eof         while (cin >> n)
+#define forcase     for(kase=1; kase<=tc; kase++)
+#define arrayloop   for(i=0; i<n; i++)
+#define cinstr      cin >> str
+#define getstr      getline (cin,str)
+#define pcase       pf ("Case %d: ",kase)
+#define pii         pair <int,int>
+#define pb          push_back
+#define in          insert
+#define llu         unsigned long long
+#define lld         long long
+#define U           unsigned int
+#define endl        "\n"
+#define TarikulCSE  int main()
+#define signoff     return 0
 
 const int num = 1500001;
 bool check[1500001];
